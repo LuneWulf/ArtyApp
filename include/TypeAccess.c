2fa35d0ac501e5871e6d30e069d293b9187d9e29
LuneWulf <108693311+LuneWulf@users.noreply.github.com>
2025-07-23 00:39:57 +0200

    Update TypeAccess.c

--- include/TypeAccess.c
+++ include/TypeAccess.c
@@ -1,52 +1,45 @@
-int ListAllAmmo(int Type) {
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
 
-    // Ammo types are listed in series but with IDs starting from 1 NOT 0.
+#include "Tools.c"
 
-    if (MASTER.Types[Type - 1].AmmoCount == 0) {
-        return -1; // ERROR: No ammo defined for the type.
+#ifndef TYPEACCESS_H
+
+#define TYPEACCESS_H
+
+void ListCharges(int Type, int Ammo) {
+    
+    for (int Charge = 0; Charge < MAX_CHARGES && MASTER.Types[Type - 1].Ammo[Ammo - 1].Charge[Charge] != 0; Charge++) {
+        printf("    CHARGE: %d, MAX RANGE: %ld\n", Charge + 1, MASTER.Types[Type - 1].Ammo[Ammo - 1].ChargeMaxRange[Charge]);
     }
+
+    printf("\n");
+
+}
+
+void ListAmmo(int Type) {
 
     for (int Ammo = 0; Ammo < MASTER.Types[Type - 1].AmmoCount; Ammo++) {
         printf("        AMMO NAME: %s, AMMO ID: %d, AMMO CHARGE COUNT: %d\n", MASTER.Types[Type - 1].Ammo[Ammo].Name, Ammo + 1, MASTER.Types[Type - 1].Ammo[Ammo].ChargeCount);
     }
 
     printf("\n");
-
-    return 0;
 }
 
-int ListAllTypes(int Set) {
-
-    // Types are listed in series but with IDs starting from 1 NOT 0.
-    
-    if (MASTER.TypeCount == 0) {
-        return - 1; // ERROR: No types defined.
-    }
+void ListAllTypes(bool List_Ammo) {
 
     for (int Type = 0; Type < MAX_TYPES && MASTER.Types[Type].AmmoCount != 0; Type++) {
         printf("    TYPE NAME: %s, TYPE ID: %d, TYPE AMMO COUNT: %d\n", MASTER.Types[Type].Name, Type + 1, MASTER.Types[Type].AmmoCount);
-        if (Set == 1) {
-            ListAllAmmo(Type + 1);
+        if (List_Ammo == true) {
+            ListAmmo(Type + 1);
         }
     }
 
     printf("\n");
 
-    return 0;
 }
 
-int ListCharges(int Type, int Ammo) {
-
-    // Ammo types are listed in series but with IDs starting from 1 NOT 0.
-
-    if (MASTER.Types[Type].AmmoCount == 0) {
-        return -1; // ERROR: No ammo defined for the type therefore no charges exist.
-                   //        However, there will always be at least 1 charge for a defined ammo.
-    }
-    
-    for (int Charge = 0; Charge < MAX_CHARGES && MASTER.Types[Type - 1].Ammo[Ammo - 1].Charge[Charge] != 0; Charge++) {
-        printf("        CHARGE: %d, MAX RANGE: %ld\n", Charge + 1, MASTER.Types[Type - 1].Ammo[Ammo - 1].ChargeMaxRange[Charge]);
-    }
-
-    return 0;
-}
+#endif
