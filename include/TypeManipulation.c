55942a4b770725d2124f368b7b4c65386e9666f3
LuneWulf <108693311+LuneWulf@users.noreply.github.com>
2025-07-23 00:40:12 +0200

    Update TypeManipulation.c

--- include/TypeManipulation.c
+++ include/TypeManipulation.c
@@ -1,52 +1,200 @@
-int ListAllAmmo(int Type) {
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
 
-    // Ammo types are listed in series but with IDs starting from 1 NOT 0.
+#include "Tools.c"
+#include "Structs.c"
+#include "Ballistics.c"
 
-    if (MASTER.Types[Type - 1].AmmoCount == 0) {
-        return -1; // ERROR: No ammo defined for the type.
-    }
+#ifndef TYPEMANIPULATION_H
 
-    for (int Ammo = 0; Ammo < MASTER.Types[Type - 1].AmmoCount; Ammo++) {
-        printf("        AMMO NAME: %s, AMMO ID: %d, AMMO CHARGE COUNT: %d\n", MASTER.Types[Type - 1].Ammo[Ammo].Name, Ammo + 1, MASTER.Types[Type - 1].Ammo[Ammo].ChargeCount);
-    }
+#define TYPEMANIPULATION_H
 
-    printf("\n");
+void ChargeMaxRange(int type, int ammo, int charge) {
 
-    return 0;
+    struct FireData Data;
+
+    Data = Simulate(0, 0, Atmosphere.WindDir, type, ammo, charge, 800, 1);
+
+    MASTER.Types[type - 1].Ammo[ammo - 1].ChargeMaxRange[charge - 1] = Data.Distance;
+
 }
 
-int ListAllTypes(int Set) {
+int AddCharge(int Type, int Ammo, bool CanCancel) {
 
-    // Types are listed in series but with IDs starting from 1 NOT 0.
-    
-    if (MASTER.TypeCount == 0) {
-        return - 1; // ERROR: No types defined.
-    }
+    char Answer = 'N';
+    float MuzzleVelocity;
 
-    for (int Type = 0; Type < MAX_TYPES && MASTER.Types[Type].AmmoCount != 0; Type++) {
-        printf("    TYPE NAME: %s, TYPE ID: %d, TYPE AMMO COUNT: %d\n", MASTER.Types[Type].Name, Type + 1, MASTER.Types[Type].AmmoCount);
-        if (Set == 1) {
-            ListAllAmmo(Type + 1);
+    while (Answer != 'Y') {
+        
+        if (CanCancel) {
+            printf("    ENTER CHARGE %d MUZZLE VELOCITY (ENTER 0 TO CANCEL): ", MASTER.Types[Type - 1].Ammo[Ammo - 1].ChargeCount + 1);
+        } else {
+            printf("    ENTER CHARGE %d MUZZLE VELOCITY: ", MASTER.Types[Type - 1].Ammo[Ammo - 1].ChargeCount + 1); 
+        }
+
+        scanf("%f", &MuzzleVelocity);
+        printf("\n");
+        clear();
+        
+        if (MuzzleVelocity == 0 && CanCancel) {
+            return 0;
+        } if (MuzzleVelocity <= 0) {
+            printf("    ERROR: MUZZLE VELOCITY MUST BE GREATER THAN 0!\n\n");
+        } else {
+            while (true) {
+                printf("    CONFIRM \"%f\" IS CORRECT (Y/N): ", MuzzleVelocity);
+                scanf("%1c", &Answer);
+                printf("\n");
+                clear();
+
+                if (Answer == 'Y' || Answer == 'N') {
+                    break;
+                } else if (Answer != 'N') {
+                    printf("    ERROR: ANSWER MUST BE Y OR N!\n\n");
+                }
+            }
         }
     }
 
-    printf("\n");
+    MASTER.Types[Type - 1].Ammo[Ammo - 1].Charge[MASTER.Types[Type - 1].Ammo[Ammo - 1].ChargeCount] = MuzzleVelocity;
 
-    return 0;
+    MASTER.Types[Type - 1].Ammo[Ammo - 1].ChargeCount++;
+
+    ChargeMaxRange(Type, Ammo, MASTER.Types[Type].Ammo[Ammo].ChargeCount);
+
+    return 1;
 }
 
-int ListCharges(int Type, int Ammo) {
+int AddAmmo(int Type, bool CanCancel) {
 
-    // Ammo types are listed in series but with IDs starting from 1 NOT 0.
+    // Adds a new ammo type at the end of the type's ammo array,
+    // then adds at least 1 charge.
+    
+    float AirFriction;
 
-    if (MASTER.Types[Type].AmmoCount == 0) {
-        return -1; // ERROR: No ammo defined for the type therefore no charges exist.
-                   //        However, there will always be at least 1 charge for a defined ammo.
+    char AmmoName[50];
+
+    if (CanCancel) {
+        InputString("ENTER AMMO TYPE NAME (MAX 50 CHARACTERS, ENTER 0 TO CANCEL): ", AmmoName);
+
+        if (*AmmoName == '0') {
+            return 0;
+        }
+    } else {
+        InputString("ENTER AMMO TYPE NAME (MAX 50 CHARACTERS): ", AmmoName);
+    }
+
+    MASTER.Types[Type - 1].AmmoCount++;
+
+    int Ammo = MASTER.Types[Type - 1].AmmoCount;
+
+    strcpy(MASTER.Types[Type - 1].Ammo[Ammo - 1].Name, AmmoName);
+
+    AirFriction = Inputfloat("ENTER AMMO AIR FRICTION: ", -100, 0);
+
+    MASTER.Types[Type - 1].Ammo[Ammo - 1].AirFriction = AirFriction;
+
+    printf("    ><>< DEFINE CHARGES ><><\n\n");
+    printf("    TO STOP DEFINING CHARGES ENTER 0 INSTEAD OF A VELOCITY\n");
+    printf("    MINIMUM OF 1 CHARGE MAXIMUM 20 CHARGES:\n\n");
+
+    AddCharge(Type, Ammo, false);
+
+    int test;
+
+    for (int i = 1; i < MAX_CHARGES; i++) {
+        test = AddCharge(Type, Ammo, true);
+
+        if (test == 0) {
+            break;
+        } 
+    }
+
+    return 1;
+}
+
+void AddType(void) {
+
+    char TypeName[50], Answer;
+    
+    InputString("ENTER TYPE NAME (MAX 50 CHARACTERS, ENTER 0 TO CANCEL): ", TypeName);
+
+    if (*TypeName == '0') {
+        return;
+    }
+
+    MASTER.TypeCount++;
+
+    strcpy(MASTER.Types[MASTER.TypeCount - 1].Name, TypeName);
+
+    printf("    ><>< DEFINE A NEW AMMO TYPE FOR THE NEW TYPE ><><\n\n");
+
+    AddAmmo(MASTER.TypeCount, false);
+
+    Answer = Confirm("DO YOU WISH TO ADD ANOTHER AMMO TYPE? (Y/N): ");
+
+    for (int i = 1; i < MAX_AMMO && Answer == 'Y'; i++) {
+        
+        int test = AddAmmo(MASTER.TypeCount, true);
+
+        if (test == 0) {
+            break;
+        }
+
+        Answer = Confirm("DO YOU WISH TO ADD ANOTHER AMMO TYPE? (Y/N): ");
+    }
+}
+
+void RemoveCharge(int type, int ammo, int charge, bool ReArrange) {
+
+    MASTER.Types[type - 1].Ammo[ammo - 1].Charge[charge - 1] = 0;
+    MASTER.Types[type - 1].Ammo[ammo - 1].ChargeMaxRange[charge - 1] = 0;
+
+    for (int i = charge; charge < MASTER.Types[type - 1].Ammo[ammo - 1].ChargeCount; charge++) {
+        MASTER.Types[type - 1].Ammo[ammo - 1].Charge[i - 1] = MASTER.Types[type - 1].Ammo[ammo - 1].Charge[i];
+        MASTER.Types[type - 1].Ammo[ammo - 1].ChargeMaxRange[i - 1] = MASTER.Types[type - 1].Ammo[ammo - 1].ChargeMaxRange[i];        
+    }
+
+    MASTER.Types[type - 1].Ammo[ammo - 1].ChargeCount--;
+
+}
+
+void RemoveAmmo(int type, int ammo, bool ReArrange) {
+        
+    strcpy(MASTER.Types[type - 1].Ammo[ammo - 1].Name, "\0");
+    MASTER.Types[type - 1].Ammo[ammo - 1].AirFriction = 0;
+
+    for (int n = 0; n < MASTER.Types[type - 1].Ammo[ammo - 1].ChargeCount; n++) {
+        RemoveCharge(type, ammo, n, false);
+    }
+
+    if (ReArrange == true) {
+        for (int i = ammo; i < MASTER.Types[type - 1].AmmoCount; i++) {
+            MASTER.Types[type - 1].Ammo[i - 1] = MASTER.Types[type - 1].Ammo[i];
+        }
+    }
+
+    MASTER.Types[type - 1].AmmoCount--;
+
+}
+
+void RemoveType(int type) {
+    strcpy(MASTER.Types[type - 1].Name, "\0");
+
+    for (int i = 0; i < MASTER.Types[type - 1].AmmoCount; i++) {
+        RemoveAmmo(type, i, false);
     }
     
-    for (int Charge = 0; Charge < MAX_CHARGES && MASTER.Types[Type - 1].Ammo[Ammo - 1].Charge[Charge] != 0; Charge++) {
-        printf("        CHARGE: %d, MAX RANGE: %ld\n", Charge + 1, MASTER.Types[Type - 1].Ammo[Ammo - 1].ChargeMaxRange[Charge]);
+    // Move back all other types one place if they had
+    // a larger id than the removed type so no gaps exist.
+    for (int i = type; i < MASTER.TypeCount; i++) {
+        MASTER.Types[i - 1] = MASTER.Types[i];
     }
 
-    return 0;
+    MASTER.TypeCount--;
 }
+
+#endif
